5ba8d89ffe5fb3f0e70fe77ad9dd15c99a334b33
0xlitf <0xlitf@gmail.com>
2024-09-05 14:06:48 +0800

    python 3.12, utf-8

--- trans.py
+++ trans.py
@@ -22,7 +22,7 @@ def trans(name):
 
     output_path = os.path.join('./poetry/', name)
 
-    with open(output_path, 'w') as f:
+    with open(output_path, 'w', encoding='utf-8') as f:
         f.write(content)
     
     
